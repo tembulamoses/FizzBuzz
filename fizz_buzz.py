236b50c9f52f72b529a4ca5a9ae08d4b3f8a05a2
moses <woesites@gmail.com>
2017-06-13 14:44:06 +0300

    the final fizzbuzz python file for today

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,24 @@
 def fizz_buzz(n):
 
-    
+    # a number divisible by both is logically divissible by both
+    # hence 15
+    if n is None:
+        return "please enter a whole number"
+    if isinstance(n, int):
+        if n%15 == 0:
+            # Divisible by both 3 and 5
+            return 'FizzBuzz'
+
+        elif n % 3 == 0:
+            # Divisible by 3
+            return 'Fizz'
+
+        elif n % 5 == 0:
+            # Divisible by 5
+            return 'Buzz'
+
+        else:
+            # Not divisible by wither 5 or 5
+            return n
+    else:
+        return "please enter a whole number"        
